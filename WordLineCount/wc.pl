e68e403ec62284ed1955e7c9743701fdd8f859c4
LastCaballero <goran.mladenovic@live.de>
2022-06-17 16:18:48 +0200

    Word Line Count

---
+++ WordLineCount/wc.pl
@@ -0,0 +1,15 @@
+#! /usr/bin/perl
+
+use strict;
+use warnings ;
+
+if ( not -t STDIN ) {
+	my @lines = <STDIN> ;
+	print $#lines+1, "\tSTDIN" , "\n" ;
+}
+
+for ( @ARGV ) {
+	open my $filehandle, "<", $_ ;
+	my @lines = <$filehandle> ;
+	print $#lines+1, "\t$_" , "\n" ;
+}
